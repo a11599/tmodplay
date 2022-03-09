f725ac0e7622a2a92e6cd8c5000403f89c88f580
Istvan Stikrad <istikrad@gmail.com>
2022-03-09 21:09:23 +0100

    Adjust DOSBox-X shortcut batch file

--- emu/db.cmd
+++
@@ -1,1 +0,0 @@
-C:\DOSBox-X\dosbox-x.exe -c mount c:\Dev\tmodplay\dist -conf C:\Dev\tmodplay\dosbox-x.conf
---
+++ emu/dosbox.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rem Just a shortcut to start DOSBox-X in the same environment/settings as
+rem makedb.bat. Requires DOSBox-X in C:\DOSBox-X directory.
+
+C:\DOSBox-X\dosbox-x.exe -c mount .. -conf dosbox-x.conf
