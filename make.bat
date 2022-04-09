04e0ae8ef4598e6127ad44079ad826c87a342070
Istvan Stikrad <istikrad@gmail.com>
2022-04-09 10:05:19 +0200

    Don't test program if compilation failed

--- make.bat
+++ make.bat
@@ -6,4 +6,7 @@ rem Compiles and starts test.bat for quick testing. Requires wmake accessible
 rem via PATH.
 
 wmake
+if errorlevel 1 goto :quit
 call test.bat
+
+:quit
--- makedb.bat
+++ makedb.bat
@@ -4,5 +4,8 @@ rem Compiles and starts test.bat using DOSBox-X for quick testing. Requires
 rem wmake accessible via PATH and DOSBox-X in C:\DOSBox-X directory.
 
 wmake
+if errorlevel 1 goto :quit
 call emu\env.bat
 "%dosbox%" test.bat -exit -c mount . -conf emu\dosbox-x.conf
+
+:quit
